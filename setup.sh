7ad1d859207b035191faedcab2a553af0bea0caf
[♥] A <142109781+Dark-Legends@users.noreply.github.com>
2023-11-16 09:13:52 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # Colour
+R="\033[1;31m"
 G="\033[1;32m"
+Y="\033[1;33m"
+PU="\033[1;34m"
+PI="\033[1;35m"
+B="\033[1;36m"
+W="\033[1;37m"
 Teal="\e[38;5;37m"
 # files PATH
 LOG_DIR=$PWD/logs
@@ -10,9 +16,9 @@ ILOG=$LOG_DIR/install.logs
 status_check() {
     if [ $? -eq 0 ]
     then
-        echo -e "$1 - Installed"
+        echo -e "$1 - ${G}Installed"
     else
-        echo -e "$1 - Failed!"
+        echo -e "$1 - ${R}Failed!"
     fi
 }
 mkdir -p $LOG_DIR
@@ -51,6 +57,7 @@ echo
 banner
 echo -e "\t\t\t\t\t\t\t${Teal}Version 3.O"
 echo -e "\033[1;32m"
+echo -e "\t\t${W}Code ${PI}By ${B}Manjuanth\n\n"
 termux_install
 echo -e '\n--------------------\n' >> "$ILOG"
 
@@ -60,6 +67,7 @@ sleep 1
 chmod +x linux-distro
 chmod +x Linux-distro
 mv linux-distro /data/data/com.termux/files/usr/bin
+mv remove-distro .remove-distro
 echo
 echo -e "\t${G}Successfully Installed\n\n"
 rm -rf setup.sh
